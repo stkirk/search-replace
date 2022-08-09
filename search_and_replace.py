099ef9c4268750f2ec59cc169bd8aea80c581880
stkirk <shaanekirk@gmail.com>
2022-08-09 13:51:40 -0400

    formatted file path for drag and drop user input

--- search_and_replace.py
+++ search_and_replace.py
@@ -3,4 +3,6 @@ input_file = input('Drag and drop the text file to be edited: ')
 search_string = input('What string would you like to replace? ')
 replacement_string = input('What would you like to replace it with? ')
 
-print(input_file, search_string, replacement_string)
+formatted_file_path = input_file if "'" not in input_file else input_file.replace("'", "")
+
+print(formatted_file_path)
