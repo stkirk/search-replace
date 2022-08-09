88e5455d9695d30b81297a78126cd1ae64a9d9e9
stkirk <shaanekirk@gmail.com>
2022-08-09 13:43:13 -0400

    added 3 user inputs for the text file, search param, replacement param

---
+++ search_and_replace.py
@@ -0,0 +1,6 @@
+# 3 user inputs, a text file, string to search for, string to replace the search string
+input_file = input('Drag and drop the text file to be edited: ')
+search_string = input('What string would you like to replace? ')
+replacement_string = input('What would you like to replace it with? ')
+
+print(input_file, search_string, replacement_string)
