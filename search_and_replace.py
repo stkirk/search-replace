66608bace824673a0f13ed3155d14f969a4f1643
stkirk <shaanekirk@gmail.com>
2022-08-09 15:18:18 -0400

    basic search and replace operation, overwriting file with new text is functional

--- search_and_replace.py
+++ search_and_replace.py
@@ -6,4 +6,22 @@ replacement_string = input('What would you like to replace it with? ')
 # input_file when dragged into terminal already has '' around it, input defaults to string type and the file path ends up coming in like "'/Users/shane/Desktop/find_and_replace.txt'", if those single quotes are present conditionally replace them with empty string to get valid file path
 formatted_file_path = input_file if "'" not in input_file else input_file.replace("'", "")
 
-print(formatted_file_path)
+# open file in read/write mode
+file_in = open(formatted_file_path, "r+")
+
+# read file to string value
+text = file_in.read()
+
+# replace search_string with replacement string
+new_text = text.replace(search_string, replacement_string)
+
+# clear text from file to 0 bytes --> maybe change later to sile_in.write(new_text)
+file_in.truncate(0)
+
+# write new text into file
+file_in.write(new_text)
+
+# close file
+file_in.close()
+
+print('Text successfully replaced!')
