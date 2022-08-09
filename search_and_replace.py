28362d2de1ae4af0d3dbf1dde76978b9a57eda63
stkirk <shaanekirk@gmail.com>
2022-08-09 14:04:24 -0400

    clarifying comment on file path formatting

--- search_and_replace.py
+++ search_and_replace.py
@@ -3,6 +3,7 @@ input_file = input('Drag and drop the text file to be edited: ')
 search_string = input('What string would you like to replace? ')
 replacement_string = input('What would you like to replace it with? ')
 
+# input_file when dragged into terminal already has '' around it, input defaults to string type and the file path ends up coming in like "'/Users/shane/Desktop/find_and_replace.txt'", if those single quotes are present conditionally replace them with empty string to get valid file path
 formatted_file_path = input_file if "'" not in input_file else input_file.replace("'", "")
 
 print(formatted_file_path)
